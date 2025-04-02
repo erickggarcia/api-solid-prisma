962c5d13b93a600c89bbaf26097c1b7ff19a26b0
Erick Garcia <erickgegarcia@gmail.com>
2025-04-01 21:45:29 -0300

    [test:create] add role admin to e2e tests that need to  validate check-in and create gyms

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,14 +1,18 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-    await request(app.server)
-        .post('/users')
-        .send({
+export async function createAndAuthenticateUser(app: FastifyInstance, isUserAdmin = false) {
+
+    await prisma.user.create({
+        data: {
             name: 'John Doe',
             email: 'JohnDoe@example.com',
-            password: '1234567'
-        })
+            password_hash: await hash('1234567', 6),
+            role: isUserAdmin ? 'ADMIN' : 'MEMBER'
+        }
+    })
 
     const authResponse = await request(app.server)
         .post('/sessions')
