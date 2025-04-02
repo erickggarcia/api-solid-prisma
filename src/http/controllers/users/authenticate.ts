075b7e969253beea4bbb9599527953320104c721
Erick Garcia <erickgegarcia@gmail.com>
2025-04-01 21:09:07 -0300

    [feat] create role based middleware

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -17,14 +17,21 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
         const { user } = await authenticateUseCase.execute({ email, password })
 
-        const token = await reply.jwtSign({}, {
-            sign: {
-                sub: user.id
-            }
-        })
+        const token = await reply.jwtSign(
+            {
+                role: user.role
+            },
+            {
+                sign: {
+                    sub: user.id
+                }
+            })
 
 
-        const refreshToken = await reply.jwtSign({}, {
+        const refreshToken = await reply.jwtSign(
+            {
+                role: user.role
+            }, {
             sign: {
                 sub: user.id,
                 expiresIn: '7d',
